f7c83fe9a1c4f31ec9b069bc86102a70e28cb3de
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-26 01:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let p_tag= document.createElement("p");
+p_tag.id="timer"
+document.body.append(p_tag)
+setInterval(()=>{
+	let d=new Date();
+	let day=d.getDay();
+	p_tag.innerText= day},86400)
